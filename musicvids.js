994f825b8c3ffb0172fad33b3f7e1d840ed27bef
JimmyJames188 <64692520+JimmyJames188@users.noreply.github.com>
2025-10-24 11:20:13 +0000

    added garf

---
+++ musicvids.js
@@ -0,0 +1,7 @@
+/*Abandoned for now*/
+const videoIDs = [
+    "https://youtu.be/K8GYYzzIpx8?si=1n4MILDUVVvns8S-",
+    "https://youtu.be/wsglGcaQd3g?si=qE9IiBBHvZm26jfv",
+    "https://youtu.be/1XGrEWNcbso?si=8qOjaraTm3duHkzA",
+    "https://youtu.be/x7r6XgjVdMY?si=AJVr5c_bdDkTdvcM"
+];
